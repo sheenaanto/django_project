05e1230206328900a474b8d5c61384369b92342f
sheenaanto <sheena.anto08@gmail.com>
2025-12-11 15:55:17 +0000

    Add About app with basic structure and views; update settings and URLs

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    if request.method == "GET":
+        return HttpResponse("This is a GET request.")
+    elif request.method == "POST":
+        return HttpResponse("This is a POST request.")
--- my_project/urls.py
+++ my_project/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hello_world import views as index_views
+from about import views as about_views
 urlpatterns = [
+    path('about/', about_views.about, name='about'),
     path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
